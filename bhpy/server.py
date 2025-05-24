2e891b63a42733a6f7723150e7e49f4ef5c434da
mack <mack@monke.group>
2025-05-23 19:09:14 -0700

    unknown

--- bhpy/server.py
+++ bhpy/server.py
@@ -6,13 +6,13 @@ print("Socket created")
 port = 6969
 
 s.bind(("", port))
-print("socket bound to port %s" %(port))
+print("socket bound to port %s" % (port))
 
 s.listen(5)
 print("Listening")
 
 while True:
-	c, addr = s.accept()
-	print("Connection from: ", addr)
+    c, addr = s.accept()
+    print("Connection from: ", addr)
 
-	c.close()
+    c.close()
